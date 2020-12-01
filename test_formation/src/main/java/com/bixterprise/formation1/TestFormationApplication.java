98710a410e051c48d6968d7d6369fba361f18852
Champlain <champlainmarius20@gmail.com>
2020-12-01 09:55:38 +0100

    initial commit

---
+++ test_formation/src/main/java/com/bixterprise/formation1/TestFormationApplication.java
@@ -0,0 +1,13 @@
+package com.bixterprise.formation1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFormationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFormationApplication.class, args);
+	}
+
+}
---
+++ test_formation/src/test/java/com/bixterprise/formation1/TestFormationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bixterprise.formation1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFormationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
